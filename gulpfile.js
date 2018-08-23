0641545dcc368a338a9f04ca07013564545c6a89
Rob Curtis <surreymagpie@yahoo.co.uk>
2018-08-23 10:45:21 +0100

    Update gulpfile
    Running either `gulp` or `npm start` will now open browser and allow
    reloading on either content or CSS changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
+const exec = require('child_process').exec;
 
 gulp.task('sass', function (cb) {
   gulp.src('sass/styles.sass')
@@ -10,6 +11,12 @@ gulp.task('sass', function (cb) {
 
 gulp.task('watch', function () {
   gulp.watch('sass/**/*.{sass, scss}', gulp.series('sass'));
-})
+});
 
-gulp.task('default', gulp.series( 'sass', 'watch'));
+gulp.task('serve', function () {
+  exec('hugo server -D --disableFastRender --noHTTPCache');
+  exec('firefox localhost:1313');
+  gulp.watch('sass/**/*.{sass, scss}', gulp.series('sass'));
+});
+
+gulp.task('default', gulp.series( 'sass', 'serve'));
